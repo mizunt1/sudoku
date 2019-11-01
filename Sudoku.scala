c80cd28871afc55cd9ef67789174fd4800cfa747
Mizu Nishikawa-Toomey <ms19mnt@tr02.tcs.cs.ox.ac.uk>
2019-11-01 10:26:32 +0000

    added comments

--- Sudoku.scala
+++ Sudoku.scala
@@ -14,6 +14,15 @@ import ox.cads.collection.Pool
 import ox.cads.collection.TerminationDetectingPool
 import ox.cads.collection.LockFreeStack
 
+//takes input partial
+// Pool is a trait. Traits are classes with abstract methods to be extended on
+// the Pool trait just has a method to get and add.
+// the get method returns an Option[T]. It can either return a None or Some[T] object.
+// T refers to the type of item returned. The type returned here is an object of a class
+// Partial
+// LockFreeStack is a Stack in shared memory which gets "locked" when things are being poped or added
+// class PooledStack therefore harneces the None return type of Pool and also 
+// the thread safeness stack. 
 class PooledStack[Partial] extends LockFreeStack[Partial] with Pool[Partial]  {
   def add(partial: Partial){
     this.push(partial)
@@ -30,6 +39,7 @@ object Sudoku{
   /** Solve the puzzle defined by init */
   def solve(init: Partial){
     // Stack to store partial solutions that we might back-track to.
+    // initialise the Stack with the first Partial, read in from file
     val stack = new scala.collection.mutable.Stack[Partial]
     stack.push(init)
     var done = false
@@ -52,41 +62,47 @@ object Sudoku{
   }
 
   def solveConcurrent(init: Partial){
-    // Stack to store partial solutions that we might back-track to.
-    //val stack = new scala.collection.mutable.Stack[Partial]
+    // This function includes the algorithm to solve the sudoku
+    // it also runs the function and splits it up in to many threads
+
     val num_workers = 10
-    // stack will either be None, or a Partial
-    // PooledStack class extends Pool which is a stack which can return a None value
-    // if there is nothing in Pool
+    // create new PooledStack object
     val stack = new PooledStack[Partial]
-    // change this PooledStack object in to another object, the TDP
-    //
-    val terminationDetectingPool = new TerminationDetectingPool(stack, num_workers)
+    // add the first partial solution to the stack
     stack.add(init)
+    // terminationDetectingPool takes a Pool object which is being called by many threads
+    // A value will be returned from the TDP 
+    // or None will be returned if all threads are returning None
+    val terminationDetectingPool = new TerminationDetectingPool(stack, num_workers)
     val done = new AtomicBoolean(false)
+    // allows for all workers to be stopped when solution is found.
+
+    // create the worker function which will be threaded
     def worker = {
     while(!done.get()){
-      // tried to get from the pool until a value us available.
-      // if not available, returns None
       val val_pop = terminationDetectingPool.get
       if(val_pop == None){done.set(true)}
       else {val partial = val_pop.get
       if(partial.complete){  // done!
+        // a partial object will return complete if
+        // all values are not zero in the grid
 	if(done.compareAndSet(false, true)){ partial.printPartial}
       }
       else{
-	// Choose position to play
+	// Choose position to play if partial is not complete
 	val(i,j) = partial.nextPos;
 	// Consider all values to play there
 	for(d <- 1 to 9)
 	  if(partial.canPlay(i,j,d)){
 	    val p1 = partial.play(i,j,d); terminationDetectingPool.add(p1)
+            // Add to stack if the number can go in that position
 
           }
 	  }
       }}
     } // end of while
-  ox.cads.util.ThreadUtil.runSystem(num_workers, worker)}
+  ox.cads.util.ThreadUtil.runSystem(num_workers, worker)
+}
 
   /** A list of files containing possible puzzles */
   private val allPossibleFiles = 
